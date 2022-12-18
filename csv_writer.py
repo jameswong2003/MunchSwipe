52380189ca7a294cdf6942ecec96b963a3d509b9
James <wong.james2003@gmail.com>
2022-12-17 19:25:43 -0500

    added cs_writer to incorporate database

---
+++ csv_writer.py
@@ -0,0 +1,9 @@
+import csv
+
+class CSV_Retrieve:
+    def __init__(self, file) -> None:
+        self.file = file
+
+    def add_restaurant(self, restaurant_info=None):
+        with open(self.file, 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
