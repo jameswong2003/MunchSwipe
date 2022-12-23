f45c896e0bfa990ac44081b82e6ed1985474f302
James <wong.james2003@gmail.com>
2022-12-22 19:14:52 -0500

    small changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 YELP_HOST = 'https://api.yelp.com/v3'
 API_KEY = 'uOLbbY-fBlAGs1rdFA9g-LMxHuQoqrcbnPI1t_tIDvVMCF6WUrsE6TQYAatNCP6M3gB9rNKcB6rAhyIlR09j4MsE42iw7eCAeJdOJde0bEj4yn4tQnfXJv4M6fGkY3Yx'
 
-response = requests.get(YELP_HOST, auth=)
+response = requests.get(YELP_HOST)
 data = ['McDonalds', '$', '4/5', 'my house']
 write_data = csv_writer.CSV_Retrieve('database/data.csv')
 write_data.add_restaurant(data)
