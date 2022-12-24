85c9935bb06a50596ec0c3f1151b637d8c33f707
James <wong.james2003@gmail.com>
2022-12-24 00:38:51 -0500

    added comments

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import csv_writer
 import requests
 
 YELP_HOST = 'https://api.yelp.com/v3'
+SEARCH_PATH = '/businesses/search'
 API_KEY = 'uOLbbY-fBlAGs1rdFA9g-LMxHuQoqrcbnPI1t_tIDvVMCF6WUrsE6TQYAatNCP6M3gB9rNKcB6rAhyIlR09j4MsE42iw7eCAeJdOJde0bEj4yn4tQnfXJv4M6fGkY3Yx'
 
-response = requests.get(YELP_HOST)
-data = ['McDonalds', '$', '4/5', 'my house']
-write_data = csv_writer.CSV_Retrieve('database/data.csv')
-write_data.add_restaurant(data)
+
+def request(host, path, api_key, url_params=None):
+    """
+    Given your API_KEY, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
