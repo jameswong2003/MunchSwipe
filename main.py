3c5c4a4b37b6451154edb9ababc0c2ce27733f80
James <wong.james2003@gmail.com>
2022-12-26 15:59:56 -0500

    added a data output file

--- main.py
+++ main.py
@@ -1,52 +1,9 @@
-import requests
-import urllib
-from urllib.parse import quote
-
-API_HOST = 'https://api.yelp.com/v3'
-SEARCH_PATH = '/businesses/search'
+from yelp_api import Yelp
+import json
 
 API_KEY = 'uOLbbY-fBlAGs1rdFA9g-LMxHuQoqrcbnPI1t_tIDvVMCF6WUrsE6TQYAatNCP6M3gB9rNKcB6rAhyIlR09j4MsE42iw7eCAeJdOJde0bEj4yn4tQnfXJv4M6fGkY3Yx'
 
-SEARCH_LIMIT = 3
+obj_dump = Yelp.search(api_key=API_KEY, term='Pasta', location='New York')
 
-def request(host, path, api_key, url_params=None):
-    """
-    Given your API_KEY, send a GET request to the API.
-    Args:
-        host (str): The domain host of the API.
-        path (str): The path of the API after the domain.
-        API_KEY (str): Your API Key.
-        url_params (dict): An optional set of query parameters in the request.
-    Returns:
-        dict: The JSON response from the request.
-    Raises:
-        HTTPError: An error occurs from the HTTP request.
-    """
-    url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    headers = {
-        'Authorization': 'Bearer %s' % api_key,
-    }
-
-    print(u'Querying {0} ...'.format(url))
-
-    response = requests.request('GET', url, headers=headers, params=url_params)
-    return response.json()
-
-def search(api_key, term, location):
-    """Query the Search API by a search term and location.
-    Args:
-        term (str): The search term passed to the API.
-        location (str): The search location passed to the API.
-    Returns:
-        dict: The JSON response from the request.
-    """
-
-    url_params = {
-        'term': term.replace(' ', '+'),
-        'location': location.replace(' ', '+'),
-        'limit': SEARCH_LIMIT
-    }
-    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
-
-print(search(API_KEY, 'Pasta', 'New York'))
+with open('database/data.json', 'w') as outfile:
+    json.dump(obj_dump, outfile, indent=4)
---
+++ yelp_api.py
@@ -0,0 +1,50 @@
+import requests
+from urllib.parse import quote
+
+API_HOST = 'https://api.yelp.com/v3'
+SEARCH_PATH = '/businesses/search'
+
+API_KEY = 'uOLbbY-fBlAGs1rdFA9g-LMxHuQoqrcbnPI1t_tIDvVMCF6WUrsE6TQYAatNCP6M3gB9rNKcB6rAhyIlR09j4MsE42iw7eCAeJdOJde0bEj4yn4tQnfXJv4M6fGkY3Yx'
+
+SEARCH_LIMIT = 3
+
+class Yelp:
+    def request(host, path, api_key, url_params=None):
+        """
+        Given your API_KEY, send a GET request to the API.
+        Args:
+            host (str): The domain host of the API.
+            path (str): The path of the API after the domain.
+            API_KEY (str): Your API Key.
+            url_params (dict): An optional set of query parameters in the request.
+        Returns:
+            dict: The JSON response from the request.
+        Raises:
+            HTTPError: An error occurs from the HTTP request.
+        """
+        url_params = url_params or {}
+        url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+        headers = {
+            'Authorization': 'Bearer %s' % api_key,
+        }
+
+        print(u'Querying {0} ...'.format(url))
+
+        response = requests.request('GET', url, headers=headers, params=url_params)
+        return response.json()
+
+    def search(api_key, term, location):
+        """Query the Search API by a search term and location.
+        Args:
+            term (str): The search term passed to the API.
+            location (str): The search location passed to the API.
+        Returns:
+            dict: The JSON response from the request.
+        """
+
+        url_params = {
+            'term': term.replace(' ', '+'),
+            'location': location.replace(' ', '+'),
+            'limit': SEARCH_LIMIT
+        }
+        return Yelp.request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
