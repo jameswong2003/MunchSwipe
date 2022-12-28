b64c46a06a0e2add03e9e3313229fcbee505d960
James <wong.james2003@gmail.com>
2022-12-27 22:23:58 -0500

    added functionality to choose a random entry from the data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from yelp_api import Yelp
 import json
+import random
+
 
 API_KEY = 'uOLbbY-fBlAGs1rdFA9g-LMxHuQoqrcbnPI1t_tIDvVMCF6WUrsE6TQYAatNCP6M3gB9rNKcB6rAhyIlR09j4MsE42iw7eCAeJdOJde0bEj4yn4tQnfXJv4M6fGkY3Yx'
 
@@ -7,3 +9,8 @@ obj_dump = Yelp.search(api_key=API_KEY, term='Pasta', location='New York')
 
 with open('database/data.json', 'w') as outfile:
     json.dump(obj_dump, outfile, indent=4)
+
+# Choose a random restaurant from the data.json file
+infile = open('database/data.json')
+data = json.load(infile)
+print(random.choice(data['businesses']))
