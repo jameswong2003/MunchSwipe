e6b0cda7fe8e667be445a259b6d6581db28533bf
James <wong.james2003@gmail.com>
2023-01-04 20:54:10 -0500

    more edits

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/result")
-def result():
-    return result
+if __name__ == "__main__":
+    app.run(debug=True)
