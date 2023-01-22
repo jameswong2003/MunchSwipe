fe9a12f1edc8c20b8fd59935524eee2fb3a1ee48
James <wong.james2003@gmail.com>
2023-01-22 18:16:20 -0500

    using a database

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request, url_for
 from yelp_api import Yelp
+import json
 
 # Configuration for API Key
 import config
 API_KEY = config.api_key
 
+database_path = 'database/data.json'
 app = Flask(__name__)
 
 # Index Page
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        print(request.form['location'])
-        # return redirect('/result')
+        obj_dump = Yelp.search(api_key=API_KEY, term=request.form['term'], location=request.form['location'])
+        # Dump data into a database
+        with open(database_path, 'w') as outfile:
+            json.dump(obj_dump, outfile, indent=4)
+        return redirect('/result')
     return render_template('index.html')
 
 # Result Page for results
 @app.route('/result')
 def result():
-    obj_dump = Yelp.search(API_KEY, term='', location='')
-    return 'hello world'
+    with open(database_path, 'r') as f:
+        data = json.load(f)
+    return data
 
 if __name__ == "__main__":
     app.run(debug=True)
