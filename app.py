aa30722e18aeaa7bb64ec4018368dfd133140cf1
James <wong.james2003@gmail.com>
2023-01-22 18:07:23 -0500

    add requirements.txt and made some more changes

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, redirect
 from yelp_api import Yelp
-import config
 
+# Configuration for API Key
+import config
 API_KEY = config.api_key
 
 app = Flask(__name__)
 
-
+# Index Page
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('intdex.html')
+    if request.method == 'POST':
+        print(request.form['location'])
+        # return redirect('/result')
+    return render_template('index.html')
+
+# Result Page for results
+@app.route('/result')
+def result():
+    obj_dump = Yelp.search(API_KEY, term='', location='')
+    return 'hello world'
 
 if __name__ == "__main__":
     app.run(debug=True)
