a45e33a27afb55513a28baec5d646ff00758ea65
James <wong.james2003@gmail.com>
2023-01-26 16:26:02 -0500

    implemented random choice to choose singular restaurants

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
 from yelp_api import Yelp
 import json
+import random
 
 # Configuration for API Key
 import config
@@ -18,14 +19,17 @@ def index():
         with open(database_path, 'w') as outfile:
             json.dump(obj_dump, outfile, indent=4)
         return redirect('/result')
+    
     return render_template('index.html')
 
 # Result Page for results
 @app.route('/result')
 def result():
+    # Choose random result from database
     with open(database_path, 'r') as f:
         data = json.load(f)
-    return data
+        result = random.choice(data['businesses'])
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
